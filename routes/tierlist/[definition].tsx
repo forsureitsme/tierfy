dec43a0422fd5a80a9e0791c01e51d37e3491927
Pedro Cardoso da Silva (@forsureitsme) <forsureitsme@gmail.com>
2024-06-08 20:27:36 -0300

    Prepare code for react-query

--- routes/tierlist/[definition].tsx
+++ routes/tierlist/[definition].tsx
@@ -1,9 +1,8 @@
 import { PageProps } from "$fresh/server.ts";
-import { existsSync } from "$std/fs/exists.ts";
-import { join } from "$std/path/join.ts";
 import { FreshContext } from "$fresh/src/server/types.ts";
 import { ITierlist } from "@/types.d.ts";
 import { Tierlist } from "@/islands/Tierlist.tsx";
+import { getTierlistDefinition } from "@/services/tierlist.ts";
 
 interface State {
   definition: ITierlist;
@@ -11,18 +10,8 @@ interface State {
 
 export async function handler(_req: Request, ctx: FreshContext<State>) {
   const { definition } = ctx.params;
-  const filePath = join(
-    Deno.cwd(),
-    "static",
-    "tierlists",
-    "definitions",
-    `${definition}.json`,
-  );
-  if (!existsSync(filePath)) {
-    throw new Deno.errors.NotFound();
-  }
-
-  ctx.state.definition = JSON.parse(Deno.readTextFileSync(filePath));
+  
+  ctx.state.definition = getTierlistDefinition(definition);
 
   return await ctx.render();
 }
